47e421d4c869f05d202b138f13424244f9a4aa79
Ildar007 <70015351+Ildar007@users.noreply.github.com>
2021-03-24 21:10:54 +0400

    deploy to orin repository

---
+++ precache-manifest.cb03ae88a61cdac69f45750e79264472.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56edc9e93831ce48d2289a783c63f16c",
+    "url": "/demo-pool/index.html"
+  },
+  {
+    "revision": "166d79f162389d26f1e0",
+    "url": "/demo-pool/static/css/3.829a7c97.chunk.css"
+  },
+  {
+    "revision": "c49b3cf9d03cfb0a685c",
+    "url": "/demo-pool/static/css/4.32715f2a.chunk.css"
+  },
+  {
+    "revision": "41bc8b8cd1f233eb2021",
+    "url": "/demo-pool/static/css/main.5ba88f12.chunk.css"
+  },
+  {
+    "revision": "406f0477cd113e77b838",
+    "url": "/demo-pool/static/js/2.e3c1bb80.chunk.js"
+  },
+  {
+    "revision": "166d79f162389d26f1e0",
+    "url": "/demo-pool/static/js/3.29c43af2.chunk.js"
+  },
+  {
+    "revision": "c49b3cf9d03cfb0a685c",
+    "url": "/demo-pool/static/js/4.4c87f7df.chunk.js"
+  },
+  {
+    "revision": "41bc8b8cd1f233eb2021",
+    "url": "/demo-pool/static/js/main.c2cbf9c5.chunk.js"
+  },
+  {
+    "revision": "5fa2bc6784d1a0f535f2",
+    "url": "/demo-pool/static/js/runtime~main.82d498aa.js"
+  },
+  {
+    "revision": "6e7c9db7e1f100edac7c6e48714d2ea0",
+    "url": "/demo-pool/static/media/preloader.6e7c9db7.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/demo-pool/static/media/user.8b1dbb1f.png"
+  }
+]);
